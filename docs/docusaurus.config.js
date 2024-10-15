Pinnacle-Networks/Pinnacle

Add robots txt and edit url in docusauraus (#2381)

docs/docusaurus.config.js
@@ -194,7 +194,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/pinnacle-io/pinnacle/blob/main/docs/hr',
+            'https://github.com/pinnacle-io/pinnacle/edit/main/docs/',
         },
         blog: false,
         theme: {

blythed/superduper

docs/docusaurus.config.js
@@ -194,7 +194,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/superduper-io/superduper/blob/main/docs/hr',
+            'https://github.com/superduper-io/superduper/edit/main/docs/',
         },
         blog: false,
         theme: {
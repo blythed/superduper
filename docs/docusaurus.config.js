Pinnacle-Networks/Pinnacle

Docs update links and texts (#2361)

docs/docusaurus.config.js
@@ -267,7 +267,7 @@ const config = {
             items: [
               {
                 label: 'Website',
-                href: 'https://pinnacle.com',
+                href: 'https://pinnacle.io',
               },
               {
                 label: 'Documentation',
@@ -288,15 +288,15 @@ const config = {
             items: [
               {
                 label: 'GitHub',
-                href: 'https://github.com/pinnacle.io/pinnacle',
+                href: 'https://github.com/pinnacle-io/pinnacle',
               },
               {
                 label: 'Issues',
-                href: 'https://github.com/pinnacle.io/pinnacle/issues',
+                href: 'https://github.com/pinnacle-io/pinnacle/issues',
               },
               {
                 label: 'Discussions',
-                href: 'https://github.com/pinnacle.io/pinnacle/discussions',
+                href: 'https://github.com/pinnacle-io/pinnacle/discussions',
               },
             ],
           },
@@ -304,20 +304,24 @@ const config = {
             title: 'Community',
             items: [
               {
+                label: 'Join.com / Careers',
+                href: 'https://join.com/companies/pinnacle',
+              },
+              {
                 label: 'LinkedIn',
-                href: 'https://www.linkedin.com/company/pinnacle/',
+                href: 'https://www.linkedin.com/company/pinnacle-io',
               },
               {
                 label: 'Slack',
-                href: 'https://pinnacle.slack.com/',
+                href: 'https://join.slack.com/t/pinnacle-public/shared_invite/zt-2ntiwsc8l-tLnhgW0wnpLlZL800~rqAg',
               },
               {
                 label: 'X / Twitter',
-                href: 'https://twitter.com/pinnacle',
+                href: 'https://twitter.com/pinnacledb',
               },
               {
                 label: 'Youtube',
-                href: 'https://www.youtube.com/@pinnacle.io',
+                href: 'https://www.youtube.com/@pinnacle-io',
               },
             ],
           },

docs/sidebars.js
@@ -2,13 +2,16 @@
 
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
-  useCasesSidebar: [{
-    type: 'autogenerated', dirName: 'use_cases'
-  }],
+  useCasesSidebar: [
+    {
+      type: 'autogenerated',
+      dirName: 'use_cases',
+    },
+  ],
   tutorialSidebar: [
     {
       type: 'doc',
-      label: 'Welcome',
+      label: 'Introduction',
       id: 'intro',
     },
     {
@@ -16,10 +19,7 @@ const sidebars = {
       label: 'Get started',
       collapsed: true,
       collapsible: true,
-      items: [
-        'get_started/installation',
-        'get_started/configuration',
-      ],
+      items: ['get_started/installation', 'get_started/configuration'],
       link: {
         type: 'doc',
         id: 'get_started/first_steps',
@@ -39,7 +39,7 @@ const sidebars = {
         'tutorials/training',
         'tutorials/custom_serialization',
         'tutorials/eager_mode',
-      ]
+      ],
     },
     {
       type: 'category',
@@ -48,11 +48,7 @@ const sidebars = {
         type: 'doc',
         id: 'core_api/intro',
       },
-      items: [
-        'core_api/connect',
-        'core_api/apply',
-        'core_api/execute',
-      ]
+      items: ['core_api/connect', 'core_api/apply', 'core_api/execute'],
     },
     {
       type: 'category',
@@ -76,7 +72,7 @@ const sidebars = {
         'apply_api/application',
         'apply_api/trigger',
         'apply_api/cron_job',
-      ]
+      ],
     },
     {
       type: 'category',
@@ -99,7 +95,7 @@ const sidebars = {
             'execute_api/data_encodings_and_schemas',
             'execute_api/using_hybrid_storage_to_handle_large_data_blobs',
             'execute_api/referring_to_data_from_diverse_sources',
-          ]
+          ],
         },
         {
           type: 'category',
@@ -108,10 +104,7 @@ const sidebars = {
             type: 'doc',
             id: 'execute_api/select_queries',
           },
-          items: [
-            'execute_api/mongodb_queries',
-            'execute_api/sql_queries',
-          ]
+          items: ['execute_api/mongodb_queries', 'execute_api/sql_queries'],
         },
         {
           type: 'category',
@@ -125,12 +118,12 @@ const sidebars = {
             'execute_api/vector_search_queries',
             'execute_api/native_vector_search',
             'execute_api/sidecar_index_vector_search',
-          ]
+          ],
         },
         'execute_api/update_queries',
         'execute_api/delete_queries',
         'execute_api/predictions',
-      ]
+      ],
     },
     {
       type: 'category',
@@ -148,7 +141,7 @@ const sidebars = {
         'models/llms',
         'models/embeddings',
         'models/bring_your_own_models',
-      ]
+      ],
     },
     {
       type: 'category',
@@ -199,20 +192,20 @@ const sidebars = {
             'data_integrations/snowflake',
             'data_integrations/sqlite',
             'data_integrations/duckdb',
-          ]
+          ],
         },
-        'data_integrations/pandas', 
-      ]
+        'data_integrations/pandas',
+      ],
     },
     {
       type: 'category',
       label: 'AI integrations',
       collapsed: false,
-      link:{
+      link: {
         type: 'generated-index',
         title: 'AI Integrations',
         description:
-          "Learn more about our AI Integrations which consists of AI models, AI APIs and Frameworks",
+          'Learn more about our AI Integrations which consists of AI models, AI APIs and Frameworks',
       },
       items: [
         'ai_integrations/anthropic',
@@ -226,8 +219,7 @@ const sidebars = {
         'ai_integrations/sklearn',
         'ai_integrations/transformers',
         'ai_integrations/vllm',
-      ]
-      
+      ],
     },
     {
       type: 'category',
@@ -242,7 +234,7 @@ const sidebars = {
         'fundamentals/datalayer_overview',
         'fundamentals/data_transmision',
         'fundamentals/vector_search_algorithm',
-      ]
+      ],
     },
     {
       type: 'category',
@@ -269,8 +261,7 @@ const sidebars = {
       items: [{ type: 'autogenerated', dirName: 'api' }],
       link: {
         type: 'generated-index',
-        description:
-          'Inline documentation of the pinnacle.io Python API',
+        description: 'Inline documentation of the pinnacle.io Python API',
       },
     },
     {
@@ -298,7 +289,7 @@ const sidebars = {
         {
           type: 'link',
           label: 'Source on GitHub',
-          href: 'https://github.com/pinnacle.io/pinnacle', 
+          href: 'https://github.com/pinnacle.io/pinnacle',
         },
       ],
     },

docs/src/pages/index.js
@@ -15,10 +15,7 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="docs/intro"
-          >
+          <Link className="button button--secondary button--lg" to="docs/intro">
             Read documentation
           </Link>
         </div>
@@ -31,8 +28,8 @@ export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      title="Docs - Pinnacle"
+      description="Find detailed technical documentation for Pinnacle's AI and database integration solutions. Access comprehensive guides, API references, and tutorials to effectively implement and utilize SuperDuper technologies in your projects. (Formerly SuperDuperDB)"
     >
       <HomepageHeader />
       {/* <main>

blythed/superduper

docs/docusaurus.config.js
@@ -267,7 +267,7 @@ const config = {
             items: [
               {
                 label: 'Website',
-                href: 'https://superduper.com',
+                href: 'https://superduper.io',
               },
               {
                 label: 'Documentation',
@@ -288,15 +288,15 @@ const config = {
             items: [
               {
                 label: 'GitHub',
-                href: 'https://github.com/superduper.io/superduper',
+                href: 'https://github.com/superduper-io/superduper',
               },
               {
                 label: 'Issues',
-                href: 'https://github.com/superduper.io/superduper/issues',
+                href: 'https://github.com/superduper-io/superduper/issues',
               },
               {
                 label: 'Discussions',
-                href: 'https://github.com/superduper.io/superduper/discussions',
+                href: 'https://github.com/superduper-io/superduper/discussions',
               },
             ],
           },
@@ -304,20 +304,24 @@ const config = {
             title: 'Community',
             items: [
               {
+                label: 'Join.com / Careers',
+                href: 'https://join.com/companies/superduper',
+              },
+              {
                 label: 'LinkedIn',
-                href: 'https://www.linkedin.com/company/superduper/',
+                href: 'https://www.linkedin.com/company/superduper-io',
               },
               {
                 label: 'Slack',
-                href: 'https://superduper.slack.com/',
+                href: 'https://join.slack.com/t/superduper-public/shared_invite/zt-2ntiwsc8l-tLnhgW0wnpLlZL800~rqAg',
               },
               {
                 label: 'X / Twitter',
-                href: 'https://twitter.com/superduper',
+                href: 'https://twitter.com/superduperdb',
               },
               {
                 label: 'Youtube',
-                href: 'https://www.youtube.com/@superduper.io',
+                href: 'https://www.youtube.com/@superduper-io',
               },
             ],
           },

docs/sidebars.js
@@ -2,13 +2,16 @@
 
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
-  useCasesSidebar: [{
-    type: 'autogenerated', dirName: 'use_cases'
-  }],
+  useCasesSidebar: [
+    {
+      type: 'autogenerated',
+      dirName: 'use_cases',
+    },
+  ],
   tutorialSidebar: [
     {
       type: 'doc',
-      label: 'Welcome',
+      label: 'Introduction',
       id: 'intro',
     },
     {
@@ -16,10 +19,7 @@ const sidebars = {
       label: 'Get started',
       collapsed: true,
       collapsible: true,
-      items: [
-        'get_started/installation',
-        'get_started/configuration',
-      ],
+      items: ['get_started/installation', 'get_started/configuration'],
       link: {
         type: 'doc',
         id: 'get_started/first_steps',
@@ -39,7 +39,7 @@ const sidebars = {
         'tutorials/training',
         'tutorials/custom_serialization',
         'tutorials/eager_mode',
-      ]
+      ],
     },
     {
       type: 'category',
@@ -48,11 +48,7 @@ const sidebars = {
         type: 'doc',
         id: 'core_api/intro',
       },
-      items: [
-        'core_api/connect',
-        'core_api/apply',
-        'core_api/execute',
-      ]
+      items: ['core_api/connect', 'core_api/apply', 'core_api/execute'],
     },
     {
       type: 'category',
@@ -76,7 +72,7 @@ const sidebars = {
         'apply_api/application',
         'apply_api/trigger',
         'apply_api/cron_job',
-      ]
+      ],
     },
     {
       type: 'category',
@@ -99,7 +95,7 @@ const sidebars = {
             'execute_api/data_encodings_and_schemas',
             'execute_api/using_hybrid_storage_to_handle_large_data_blobs',
             'execute_api/referring_to_data_from_diverse_sources',
-          ]
+          ],
         },
         {
           type: 'category',
@@ -108,10 +104,7 @@ const sidebars = {
             type: 'doc',
             id: 'execute_api/select_queries',
           },
-          items: [
-            'execute_api/mongodb_queries',
-            'execute_api/sql_queries',
-          ]
+          items: ['execute_api/mongodb_queries', 'execute_api/sql_queries'],
         },
         {
           type: 'category',
@@ -125,12 +118,12 @@ const sidebars = {
             'execute_api/vector_search_queries',
             'execute_api/native_vector_search',
             'execute_api/sidecar_index_vector_search',
-          ]
+          ],
         },
         'execute_api/update_queries',
         'execute_api/delete_queries',
         'execute_api/predictions',
-      ]
+      ],
     },
     {
       type: 'category',
@@ -148,7 +141,7 @@ const sidebars = {
         'models/llms',
         'models/embeddings',
         'models/bring_your_own_models',
-      ]
+      ],
     },
     {
       type: 'category',
@@ -199,20 +192,20 @@ const sidebars = {
             'data_integrations/snowflake',
             'data_integrations/sqlite',
             'data_integrations/duckdb',
-          ]
+          ],
         },
-        'data_integrations/pandas', 
-      ]
+        'data_integrations/pandas',
+      ],
     },
     {
       type: 'category',
       label: 'AI integrations',
       collapsed: false,
-      link:{
+      link: {
         type: 'generated-index',
         title: 'AI Integrations',
         description:
-          "Learn more about our AI Integrations which consists of AI models, AI APIs and Frameworks",
+          'Learn more about our AI Integrations which consists of AI models, AI APIs and Frameworks',
       },
       items: [
         'ai_integrations/anthropic',
@@ -226,8 +219,7 @@ const sidebars = {
         'ai_integrations/sklearn',
         'ai_integrations/transformers',
         'ai_integrations/vllm',
-      ]
-      
+      ],
     },
     {
       type: 'category',
@@ -242,7 +234,7 @@ const sidebars = {
         'fundamentals/datalayer_overview',
         'fundamentals/data_transmision',
         'fundamentals/vector_search_algorithm',
-      ]
+      ],
     },
     {
       type: 'category',
@@ -269,8 +261,7 @@ const sidebars = {
       items: [{ type: 'autogenerated', dirName: 'api' }],
       link: {
         type: 'generated-index',
-        description:
-          'Inline documentation of the superduper.io Python API',
+        description: 'Inline documentation of the superduper.io Python API',
       },
     },
     {
@@ -298,7 +289,7 @@ const sidebars = {
         {
           type: 'link',
           label: 'Source on GitHub',
-          href: 'https://github.com/superduper.io/superduper', 
+          href: 'https://github.com/superduper.io/superduper',
         },
       ],
     },

docs/src/pages/index.js
@@ -15,10 +15,7 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="docs/intro"
-          >
+          <Link className="button button--secondary button--lg" to="docs/intro">
             Read documentation
           </Link>
         </div>
@@ -31,8 +28,8 @@ export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      title="Docs - Superduper"
+      description="Find detailed technical documentation for Superduper's AI and database integration solutions. Access comprehensive guides, API references, and tutorials to effectively implement and utilize SuperDuper technologies in your projects. (Formerly SuperDuperDB)"
     >
       <HomepageHeader />
       {/* <main>
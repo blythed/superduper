Pinnacle-Networks/Pinnacle

remove unuse import (#2295)
Co-authored-by: Edward Li <a602281467@163.com>

deploy/databases/clickhouse/example.py
@@ -5,4 +5,6 @@ password = ''
 port = 8123
 host = 'localhost'
 
-db = pinnacle(f"clickhouse://{user}:{password}@{host}:{port}", metadata_store=f'mongomock://meta')
+db = pinnacle(
+    f"clickhouse://{user}:{password}@{host}:{port}", metadata_store=f'mongomock://meta'
+)

deploy/databases/oracle/example.py
@@ -8,16 +8,16 @@ database = 'test_db'
 
 # OK
 con = ibis.oracle.connect(
-    user = 'pinnacle',
-    password = 'pinnacle',
-    port = 1521,
-    host = 'localhost',
-    database = 'test_db',
+    user='pinnacle',
+    password='pinnacle',
+    port=1521,
+    host='localhost',
+    database='test_db',
 )
 
 # ERROR AttributeError: 'str' object has no attribute 'username'
-#con = ibis.connect(f"oracle://{user}:{password}@{host}:{port}/{database}")
+# con = ibis.connect(f"oracle://{user}:{password}@{host}:{port}/{database}")
 
 # ERROR ModuleNotFoundError: No module named 'ibis.backends.base'
 # from pinnacle import pinnacle
-#db = pinnacle(f"oracle://{user}:{password}@{host}:{port}/{database}")
+# db = pinnacle(f"oracle://{user}:{password}@{host}:{port}/{database}")

deploy/databases/postgresql/example.py
@@ -7,4 +7,4 @@ host = 'localhost'
 database = 'test_db'
 db_uri = f"postgres://{user}:{password}@{host}:{port}/{database}"
 
-db = pinnacle(db_uri, metadata_store=db_uri.replace('postgres://', 'postgresql://')) 
+db = pinnacle(db_uri, metadata_store=db_uri.replace('postgres://', 'postgresql://'))

pinnacle/components/component.py
@@ -14,7 +14,7 @@ from functools import wraps
 import yaml
 
 from pinnacle import logging
-from pinnacle.base.constant import KEY_BLOBS, KEY_BUILDS, KEY_FILES
+from pinnacle.base.constant import KEY_BLOBS, KEY_FILES
 from pinnacle.base.leaf import Leaf
 from pinnacle.jobs.job import ComponentJob, Job
 
@@ -58,31 +58,6 @@ def _build_info_from_path(path: str):
         config_object[KEY_FILES] = files
 
     return config_object
-
-
-def import_(r=None, path=None, db=None):
-    """Helper function for importing component JSONs, YAMLs, etc.
-
-    :param r: Object to be imported.
-    :param path: Components directory.
-    :param db: Datalayer instance.
-    """
-    from pinnacle.base.document import _build_leaves
-
-    if r is None:
-        try:
-            with open(f'{path}/component.json') as f:
-                r = json.load(f)
-        except FileNotFoundError:
-            with open(f'{path}/component.yaml') as f:
-                r = yaml.safe_load(f)
-        for id_ in os.listdir(path):
-            if id_ == 'component.json' or id_ == 'component.yaml':
-                continue
-            with open(f'{path}/{id_}', 'rb') as f:
-                bytes[id_] = f.read()
-    r[KEY_BUILDS] = _build_leaves(r[KEY_BUILDS], db=db)[0]
-    return r[KEY_BUILDS][r['_base']]
 
 
 def getdeepattr(obj, attr):

blythed/superduper

deploy/databases/clickhouse/example.py
@@ -5,4 +5,6 @@ password = ''
 port = 8123
 host = 'localhost'
 
-db = superduper(f"clickhouse://{user}:{password}@{host}:{port}", metadata_store=f'mongomock://meta')
+db = superduper(
+    f"clickhouse://{user}:{password}@{host}:{port}", metadata_store=f'mongomock://meta'
+)

deploy/databases/oracle/example.py
@@ -8,16 +8,16 @@ database = 'test_db'
 
 # OK
 con = ibis.oracle.connect(
-    user = 'superduper',
-    password = 'superduper',
-    port = 1521,
-    host = 'localhost',
-    database = 'test_db',
+    user='superduper',
+    password='superduper',
+    port=1521,
+    host='localhost',
+    database='test_db',
 )
 
 # ERROR AttributeError: 'str' object has no attribute 'username'
-#con = ibis.connect(f"oracle://{user}:{password}@{host}:{port}/{database}")
+# con = ibis.connect(f"oracle://{user}:{password}@{host}:{port}/{database}")
 
 # ERROR ModuleNotFoundError: No module named 'ibis.backends.base'
 # from superduper import superduper
-#db = superduper(f"oracle://{user}:{password}@{host}:{port}/{database}")
+# db = superduper(f"oracle://{user}:{password}@{host}:{port}/{database}")

deploy/databases/postgresql/example.py
@@ -7,4 +7,4 @@ host = 'localhost'
 database = 'test_db'
 db_uri = f"postgres://{user}:{password}@{host}:{port}/{database}"
 
-db = superduper(db_uri, metadata_store=db_uri.replace('postgres://', 'postgresql://')) 
+db = superduper(db_uri, metadata_store=db_uri.replace('postgres://', 'postgresql://'))

superduper/components/component.py
@@ -14,7 +14,7 @@ from functools import wraps
 import yaml
 
 from superduper import logging
-from superduper.base.constant import KEY_BLOBS, KEY_BUILDS, KEY_FILES
+from superduper.base.constant import KEY_BLOBS, KEY_FILES
 from superduper.base.leaf import Leaf
 from superduper.jobs.job import ComponentJob, Job
 
@@ -58,31 +58,6 @@ def _build_info_from_path(path: str):
         config_object[KEY_FILES] = files
 
     return config_object
-
-
-def import_(r=None, path=None, db=None):
-    """Helper function for importing component JSONs, YAMLs, etc.
-
-    :param r: Object to be imported.
-    :param path: Components directory.
-    :param db: Datalayer instance.
-    """
-    from superduper.base.document import _build_leaves
-
-    if r is None:
-        try:
-            with open(f'{path}/component.json') as f:
-                r = json.load(f)
-        except FileNotFoundError:
-            with open(f'{path}/component.yaml') as f:
-                r = yaml.safe_load(f)
-        for id_ in os.listdir(path):
-            if id_ == 'component.json' or id_ == 'component.yaml':
-                continue
-            with open(f'{path}/{id_}', 'rb') as f:
-                bytes[id_] = f.read()
-    r[KEY_BUILDS] = _build_leaves(r[KEY_BUILDS], db=db)[0]
-    return r[KEY_BUILDS][r['_base']]
 
 
 def getdeepattr(obj, attr):